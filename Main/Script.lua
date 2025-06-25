3d70e70b7dfd61bfdd6a8bb0fafee92e8f207fc7
Lunar <lunaralwaysontop@gmail.com>
2025-06-25 10:59:04 -0300

    Update Script.lua

--- Main/Script.lua
+++ Main/Script.lua
@@ -1,1 +1,21 @@
-.
+local UserInputService = game:GetService("UserInputService")
+
+local function DetectDevice()
+    if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
+        return "Mobile"
+    elseif UserInputService.KeyboardEnabled and not UserInputService.TouchEnabled then
+        return "PC"
+    else
+        return "Unknown"
+    end
+end
+
+local device = DetectDevice()
+
+if device == "PC" then
+    game.Players.LocalPlayer:Kick("Wait for release in 30/06/2025")
+elseif device == "Mobile" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/iLunar-Scripts/Lunar/refs/heads/main/Versions/LastMobileTPS.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Unknown Device")
+end
